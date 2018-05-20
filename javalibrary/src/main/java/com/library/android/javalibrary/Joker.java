e9ba01cf4c295f071687f535b935f7e0d18a560d
Reda Tawfik <reda.rm148@gmail.com>
2018-05-20 04:01:54 +0200

    secondd

--- javalibrary/src/main/java/com/library/android/javalibrary/Joker.java
+++ javalibrary/src/main/java/com/library/android/javalibrary/Joker.java
@@ -3,6 +3,6 @@ package com.library.android.javalibrary;
 public class Joker {
 
     public String getJoke(){
-        return "Very fin joke";
+        return "Yo momma is so fat, I took a picture of her last Christmas and it's still printing.";
     }
 }
